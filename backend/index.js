bb131ca860603196a9be5ef0f36d209ba8824ab6
Rahul Rai <errahulrai484@gmail.com>
2024-10-13 11:29:00 -0700

    Configure CORS to allow any frontend domain

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,14 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-// Dynamically allow Vercel URLs or any specific domain you want
 app.use(cors({
-    origin: 'https://amazon-product-scraper.vercel.app',  // Your Vercel frontend URL
-    methods: ['GET', 'POST'],  // Allowed methods
-    credentials: true,  // If you need to send cookies or auth tokens
+    origin: (origin, callback) => {
+        // Allow requests from any origin (NOT RECOMMENDED for production)
+        callback(null, true);
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true // Allow credentials
 }));
 
 app.use(express.json())
